6d1f72ff53e5745863b869e476de1e1ddb313ac6
seanyu4296 <seanyu4296@gmail.com>
2020-08-10 04:12:56 +0000

    Remove comments

--- src/Dijkstras.purs
+++ src/Dijkstras.purs
@@ -162,22 +162,6 @@ djikstra graph start end = go init.costTable init.parentsTable Set.empty
         { costTable: Map.empty, parentsTable: Map.empty }
         keys
 
--- // TODO: Create sample data
--- djikstra :: Graph -> String -> Maybe Result
--- djikstra graph start =
---   where
---   go :: CostTable -> ParentsTable -> Array String -> Maybe Result
---   go costs parents processed =
---     -- this part has to be one big do Maybe recursion
---     -- if it cannot find a connection to "fin" then Nothing
---     -- if isNothing findLowestCostNode then return result
---   -- // TODO: use first node and get connecting nodes for initial table
---   initCosts :: CostTable
---   initCosts =
---   initParents :: ParentsTable
---   initParents =
---   findLowestCostNode :: CostTable -> Array String -> Maybe String
---   findLowestCostNode costs processed =
 main :: Effect Unit
 main = do
   logShow $ djikstra samplegraph "start" "fin"
